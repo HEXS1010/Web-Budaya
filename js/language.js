39ca117963c2d6b5f91250ce5c831aa82fe1f5f3
Komang Aswameda <aswameda18@gmail.com>
2026-07-04 00:26:05 +0800

    penambahan fitur language

---
+++ js/language.js
@@ -0,0 +1,57 @@
+// const languageButtons = document.querySelectorAll(".lang-btn");
+
+// languageButtons.forEach((button) => {
+//   button.addEventListener("click", () => {
+//     console.log(button.dataset.lang);
+//   });
+// });
+
+// console.log("language.js berhasil dibaca");
+
+const languageButtons = document.querySelectorAll(".lang-btn");
+
+// Menyimpan teks Indonesia asli
+const defaultLanguage = {};
+
+document.querySelectorAll("[data-i18n]").forEach((element) => {
+  defaultLanguage[element.dataset.i18n] = element.textContent.trim();
+});
+
+// Mengambil data dari object berdasarkan key
+function getTranslation(object, path) {
+  return path.split(".").reduce((current, key) => current[key], object);
+}
+
+async function loadLanguage(lang) {
+  const elements = document.querySelectorAll("[data-i18n]");
+
+  // Kembali ke Bahasa Indonesia
+  if (lang === "id") {
+    elements.forEach((element) => {
+      element.textContent = defaultLanguage[element.dataset.i18n];
+    });
+
+    return;
+  }
+
+  // Ambil file JSON
+  const response = await fetch(`lang/${lang}.json`);
+  const language = await response.json();
+
+  // Ganti semua teks
+  elements.forEach((element) => {
+    const key = element.dataset.i18n;
+
+    const translation = getTranslation(language, key);
+
+    if (translation) {
+      element.textContent = translation;
+    }
+  });
+}
+
+languageButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    loadLanguage(button.dataset.lang);
+  });
+});
